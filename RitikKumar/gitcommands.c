4269cda3427908b1d3ee2e4ec8925409ee035aca
Ritik30071999 <ritikkumar.kumar72@gmail.com>
2018-08-23 01:36:53 -0700

    Added Git Command file

---
+++ RitikKumar/gitcommands.c
@@ -0,0 +1,31 @@
+basic git commands
+git init- creates a new local repository
+
+git clone /path/to/repository- create a working of local repository
+
+git add <filename>- add one or more files to the staging area
+
+git commit -m "commit message"- commit changes to the head 
+
+git push origin master- send changes to the master branch of your remote repo
+
+git status- list the files you've changed and still need to add or commit
+
+git checkout -b <branchname>- create a new branch and switch to it
+
+git checkout <branchname>- switch to another branch
+
+git branch- list the branches and highlights the currently worrking on
+
+git push origin <branchname>- push the branch to your remote repo
+
+git push --all origin- pushes all the branches to your remote repo
+
+git pull- fetch and merge changes on remote server to your working directory
+
+git merge <branchname>- to merge a different branch to your active branch
+
+git add <filename>
+
+git log
+

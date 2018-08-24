4ac35df8d27a773dfdb7b0cf40d42b26a67f83c7
Ritik30071999 <ritikkumar.kumar72@gmail.com>
2018-08-23 21:48:22 -0700

    commands list file

---
+++ RitikKumar/commands.c
@@ -0,0 +1,53 @@
+some terminal commands
+
+pwd-present working directory
+
+ls-to know what files are in the directory
+
+cd .. - to go to the previous directory
+
+cd . - to be on the present directory
+
+cd- to go on the home
+
+mkdir- to create or make a new directory
+
+rmdir- to remove a particular empty directory
+
+rm- to delete files and directories
+
+touch- to create an empty file
+
+man- to know about a particular command
+
+--help- displays the commands
+
+cp- to copy files to another directory
+
+mv- to move files to another directory
+
+locate- to get the location of a particular directory
+
+echo- helps to move some data usually text to an already existing file
+
+cat-displays the content of a file
+
+nano,vi&jed- nano and vi are already installed text editors in the command line
+
+sudo- (SuperUserDo) if you want any command to be with administrative or root privileges
+
+df- to see rhe available disk space in each of the partitions 
+
+du- to know the disk usage of a file in your system
+
+tar- to compress or uncompress a zipped file
+
+zip&unzip- to compress into a zip file or unzip to extract files from a zip archive file
+
+apt-get- to install packages or to upgrade
+
+chmod- to make a file executable
+
+hostname- to know your name in your network
+
+ping- to check your connection to your network 

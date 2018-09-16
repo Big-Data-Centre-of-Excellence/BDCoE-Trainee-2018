7d0e287c069a3ce28e134ff6394ad17e890e20eb
anandkumargithub <techiehere08@gmail.com>
2018-09-16 21:54:05 +0530

    admin panel

--- Anand/Project/register.php
+++ Anand/Project/register.php
@@ -10,36 +10,36 @@
 				<h2>REGISTER</h2>
 			<input type="text" name="name" placeholder="Your Name" required="required"></br>
 			<input type="email" name="email" placeholder="Email ID" required="required" ></br>
-			<input type="number" name="number" placeholder="Mobile Number" required="required" ></br>
-			<input type="number" name="stid" placeholder="Student ID" required="required"></br>
-			<input type="number" name="year" placeholder="Year" required="required"></br>
-			<select required="required">
-					<option type="" > DAY </option>
+			<input type="text" name="number" placeholder="Mobile Number" required="required" ></br>
+			<input type="text" name="stid" placeholder="Student ID" required="required"></br>
+			<input type="number" name="year" placeholder="Year" required="required"></br></br>
+			<select required="required" name="day">
+					<option value="hu"> DAY </option>
 					<?php for($i=1;$i<32;$i++) {?>
-					
-					 <option value="<?php echo $i;?>"> <?php echo $i;?></option> 
+
+					 <option value="<?php echo $i;?>"> <?php echo $i;?></option>
 					<?php }?>
 				</option>
 			</select>
-			<select required="required">
+			<select required="required" name="month">
 					<option> MONTH </option>
 					<?php for($i="1";$i<"13";$i++) {?>
-					
-					 <option value="<?php echo $i;?>"> <?php echo $i;?></option> 
+
+					 <option value="<?php echo $i;?>"> <?php echo $i;?></option>
 					<?php }?>
 				</option>
-			</select> 
-			<select required="required">
+			</select>
+			<select required="required" name="year1">
 					<option> YEAR </option>
 					<?php for($i=2018;$i>1990;$i--) {?>
-					
-					 <option value="<?php echo $i;?>"> <?php echo $i;?></option> 
+
+					 <option value="<?php echo $i;?>"> <?php echo $i;?></option>
 					<?php }?>
 				</option>
 			</select></br>
 			<input type= "radio" name="gender" value="Male" required="required">Male
 			<input type= "radio" name="gender" value="Female" required="required">Female </br>
-			<input type= "submit" name="submit" value="SUBMIT">		
+			<input type= "submit" name="submit" value="SUBMIT">
 		</form>
 	</div>
 </body>
--- Anand/Project/registerdb.php
+++ Anand/Project/registerdb.php
@@ -1,15 +1,21 @@
-<?php 
-$con=mysqli_connect('localhost','root','','register');
+<?php
+$con=mysqli_connect('localhost','root','','data');
 $name=$_POST['name'];
 $email=$_POST['email'];
 $number=$_POST['number'];
 $stid=$_POST['stid'];
 $year=$_POST['year'];
 $gender=$_POST['gender'];
+$date1=$_POST['day'];
+$month=$_POST['month'];
+$year1=$_POST['year1'];
+$date=$year1."-".$month."-".$date1;
+echo $date;
 
-$query="INSERT INTO `register`(`name`, `email`, `number`, `stid`, `year`) VALUES ('$name','$email',
-		'$number','$stid','$year')";
+$query="INSERT INTO user (name, email, number, stid, year,gender,date) VALUES ('$name','$email',
+		'$number','$stid','$year','$gender','$date')";
 
 $run=mysqli_query($con,$query);
+
 
 ?>

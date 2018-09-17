62be527714b6721952647d1151f8605b9089f0a0
anandkumargithub <techiehere08@gmail.com>
2018-09-17 22:53:13 +0530

    email to user

--- Anand/Project/birthdaymatch.php
+++ Anand/Project/birthdaymatch.php
@@ -1,5 +1,7 @@
 <?php
 $con=mysqli_connect('localhost','root','','data');
+require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
+require '/usr/share/php/libphp-phpmailer/class.smtp.php';
 $bdate=date('d');
 $bmonth=date('m');
 $sql="SELECT * FROM `user` ";
@@ -9,14 +11,36 @@ while($row=mysqli_fetch_assoc($query))
     $id=$row['id'];
     $stid=$row['stid'];
     $date=$row['date'];
+    $email=$row['email'];
+    $name=$row['name'];
     $month = date('m', strtotime($date));
     $day=date('j',strtotime($date));
-    if($bmonth==$month && $bdate==$day)
-    {
-        echo "hell";
-    }
-    else
-        echo "lol";
+
+        if($bmonth==$month && $bdate==$day)
+    { 
+        {
+
+$mail = new PHPMailer;
+$mail->setFrom('admin@example.com');
+$mail->addAddress($email);
+$mail->Subject = 'Message sent';
+$mail->Body = 'Hello '. $name.' Wish you a very HAPPY BIRTHDAY';
+$mail->addAttachment('employees.json');
+$mail->IsSMTP();
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'ssl://smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Port = 465;
+$mail->Username = 'techiehere08@gmail.com';
+$mail->Password = '76683594457';
+if(!$mail->send()) {
+  echo 'Email is not sent to'.$name.'<br>';
+  echo 'Email error: ' . $mail->ErrorInfo;
+} else 
+  echo 'Email has been sent to '.$name.'<br>';
 }
+   }
+ }
+
 
  ?>

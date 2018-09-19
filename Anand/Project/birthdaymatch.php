91b17e589fbd02e7cdea6b1ff60507b43bd7f687
anandkumargithub <techiehere08@gmail.com>
2018-09-19 18:42:32 +0530

    fbemailsms

--- Anand/Project/birthdaymatch.php
+++ Anand/Project/birthdaymatch.php
@@ -1,7 +1,9 @@
 <?php
 $con=mysqli_connect('localhost','root','','data');
-require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
-require '/usr/share/php/libphp-phpmailer/class.smtp.php';
+require_once ('facebookapp/vendor/autoload.php');
+require_once ('gmail/vendor/autoload.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 $bdate=date('d');
 $bmonth=date('m');
 $sql="SELECT * FROM `user` ";
@@ -18,30 +20,35 @@ while($row=mysqli_fetch_assoc($query))
     $day=date('j',strtotime($date));
 
         if($bmonth==$month && $bdate==$day)
-        { 
-        //mail to the users
-          {
-            $mail = new PHPMailer;
-            $mail->setFrom('admin@example.com');
-            $mail->addAddress($email);
-            $mail->Subject = 'Message sent';
-            $mail->Body = 'Hello '. $name.' Wish you a very HAPPY BIRTHDAY';
-            $mail->addAttachment('employees.json');
-            $mail->IsSMTP();
-            $mail->SMTPSecure = 'ssl';
-            $mail->Host = 'ssl://smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Port = 465;
-            $mail->Username = 'techiehere08@gmail.com';
-            $mail->Password = '76683594457';
-            if(!$mail->send()) 
-            {
-              echo 'Email is not sent to'.$name.'<br>';
-              echo 'Email error: ' . $mail->ErrorInfo;
-            } 
-            else 
-              echo 'Email has been sent to '.$name.'<br>';
-          }
+{ 
+				        //mail to the users
+				          {
+				            
+				{
+				$to_id = $email;
+				$subject =  'Birthday Wish';
+				$message = 'Happy Birthday '.$name.', have a good day!';
+				$mail = new PHPMailer(true);
+				$mail->isSMTP();
+				$mail->Host = 'smtp.gmail.com';
+				$mail->SMTPAuth = true;
+				$mail->Username = 'techiehere08@gmail.com';
+				$mail->Password = '*******';
+				$mail->SMTPSecure = 'tls';  
+				$mail->Port = 587;
+				$mail->setFrom('techiehere08@gmail.com', 'BDCOE');
+				$mail->addAddress($to_id);
+				$mail->Subject = $subject;
+				$mail->Body = $message;
+				if(!$mail->send())
+				{
+				$error = "Mailer Error: " .$mail->ErrorInfo;
+				echo "<div class=display> '.$error.'  </div>";
+				}
+				else
+				echo " <div class=display> Message Sent </div>";
+				}
+			    }//mail over
                     {
                     //sms
                           
@@ -70,7 +77,38 @@ while($row=mysqli_fetch_assoc($query))
                 echo $response;
                 echo "SmS is sent to". $name;
   
-          }
-        }
-      }
+				      } //sms over*/
+				      {//facebook
+						$fb = new Facebook\Facebook([
+						'app_id' => '247169872653933',
+						'app_secret' => '4f0795b718156df18e9614d8b0021b3e',
+						'default_graph_version' => 'v3.1',
+						]);
+						$pageAccessToken = 'EAADgzLh1bm0BAI4iRAConbWJZCJeO0MEXZAZAzrKcHtqj25onLlwpU8w5ZCP3ZAcI9FKKRFsiSvpOnjyLb8cOzWueGXUPJ02dsg7GDkHvktRsRwIPeico4Imtg3lKcf2ZBZAYraZCbVyTyiaUzWTfOCV4A4wrG0ZCSYZCMZBDGMYuimsqCVZBnzhsVt5uZBb047ZAx4y4ZD';
+						$msg =[
+						'message' => 'Happy Birthday '.$name.', have a good day!',
+						//'source' => $fb->fileToUpload('http://localhost/test/Project/back.jpg'),
+						'url' => 'https://happybirthdaywishesworld.com/wp-content/uploads/2017/12/Happy-birthday-images.jpg',
+						];
+						try
+						{
+						$response = $fb->post('/me/photos', $msg,$pageAccessToken);
+						}
+						catch(Facebook\Exceptions\FacebookResponseException $e)
+						{
+						echo 'Graph returned an error: '.$e->getMessage();
+						exit;
+						}
+						catch(Facebook\Exceptions\FacebookSDKException $e)
+						{
+						echo 'Facebook SDK returned an Error: '.$e->getMessage();
+						exit;
+						}
+						$graphNode = $response->getGraphNode();
+						echo 'ID:'.$graphNode['id'];
+						} //facebook over
+
+	        }
+}
  ?>
+
